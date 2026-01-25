a31024feb5b03b222c7ae3c78e8f953cdac856cf
Kostin Ivan <mrironheart@gmail.com>
2026-01-25 22:36:45 +0300

    Добавил POM-класс для страницы заказа самоката

---
+++ src/test/java/pom/YandexScooterOrderPage.java
@@ -0,0 +1,176 @@
+package pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Класс страницы формирования заказа Яндекс Самокат
+ */
+public class YandexScooterOrderPage {
+    private final WebDriver driver;
+    // Поле "Имя" на первой форме заказа
+    private final By firstNameField =
+            By.xpath("//*[@class='Order_Form__17u6u']//input[contains(@placeholder, 'Имя')]");
+    // Поле "Фамилия" на первой форме заказа
+    private final By familyNameField =
+            By.xpath("//*[@class='Order_Form__17u6u']//input[contains(@placeholder, 'Фамилия')]");
+    // Поле "Адрес" на первой форме заказа
+    private final By addressField =
+            By.xpath("//*[@class='Order_Form__17u6u']//input[contains(@placeholder, 'Адрес')]");
+    // Поле "Станция метро" на первой форме заказа
+    private final By metroStationField =
+            By.xpath("//*[@class='Order_Form__17u6u']//input[contains(@placeholder, 'метро')]");
+    // Первая станция в выпадающем списке "Станция метро"
+    private final By metroStation =
+            By.xpath("//*[@class='select-search__select']//button[@value='1']");
+    // Поле "Телефон" на первой форме заказа
+    private final By telNumberField =
+            By.xpath("//*[@class='Order_Form__17u6u']//input[contains(@placeholder, 'Телефон')]");
+    // Кнопка "Далее" на первой форме заказа
+    private final By nextButton =
+            By.xpath("//button[contains(@class, 'Button_Middle__1CSJM')]");
+    // Поле "Когда привезти самокат" на второй форме заказа
+    private final By orderDateField =
+            By.xpath("//*[@class='Order_Form__17u6u']//input[contains(@placeholder, 'Когда привезти самокат')]");
+    // Текущая дата в выпадающем календаре поля "Когда привезти самокат" на второй форме заказа
+    private final By orderDate =
+            By.xpath("//*[contains(@class,'react-datepicker__day--today')]");
+    // Поле "Срок аренды" на второй форме заказа
+    private final By rentTimeField = By.className("Dropdown-placeholder");
+    // Период "сутки" в выпадающем меню "Срок аренды" на второй форме заказа
+    private final By rentTime =
+            By.xpath("//*[@class='Dropdown-option' and contains(text(),'сутки')]");
+    // Кнопка "Заказать" на второй форме заказа
+    private final By orderButton =
+            By.xpath("//*[contains(@class,'Button_Middle__1CSJM') and text()='Заказать']");
+    // Кнопка "Да" на всплывающем окне "Хотите оформить заказ?"
+    private final By confirmButton =
+            By.xpath("//*[@class='Order_Modal__YZ-d3']//button[contains(text(),'Да')]");
+    // Заголовок всплывающего окна об успешном формировании заказа
+    private final By completeOrderFormTitle = By.className("Order_ModalHeader__3FDaJ");
+
+    public YandexScooterOrderPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    /**
+     * Вспомогательный методы для заполнения значений полей на странце заказа Яндекс Самокат
+     * @param fieldName
+     */
+    private void checkAndClearField(By fieldName){
+        driver.findElement(fieldName).isEnabled();
+        driver.findElement(fieldName).clear();
+    }
+
+    /**
+     * Метод записывает в поле "Имя" переданный параметр
+     * @param newFirstName
+     */
+    public void setFirstName(String newFirstName) {
+        checkAndClearField(firstNameField);
+        driver.findElement(firstNameField).sendKeys(newFirstName);
+    }
+
+    /**
+     * Метод записывает в поле "Фамилия" переданный параметр
+     * @param newFamilyName
+     */
+    public void setFamilyName(String newFamilyName) {
+        checkAndClearField(familyNameField);
+        driver.findElement(familyNameField).sendKeys(newFamilyName);
+    }
+
+    /**
+     * Метод записывает в поле "Фамилия" переданный параметр
+     * @param newAddress
+     */
+    public void setAddress(String newAddress) {
+        checkAndClearField(addressField);
+        driver.findElement(addressField).sendKeys(newAddress);
+    }
+
+    /**
+     * Метод выбирает в поле "Станция метро" станцию
+     */
+    public void setMetroStation() {
+        driver.findElement(metroStationField).click();
+        driver.findElement(metroStation).click();
+    }
+
+    /**
+     * Метод записывает в поле "Телефон" переданный параметр
+     * @param newTelNumber
+     */
+    public void setTelNumber(String newTelNumber) {
+        checkAndClearField(telNumberField);
+        driver.findElement(telNumberField).sendKeys(newTelNumber);
+    }
+
+    /**
+     * Метод нажимает кнопку "Далее" на первой форме заказа
+     */
+    public void clickNextButton() {
+        driver.findElement(nextButton).click();
+    }
+
+    /**
+     * Метод выбирает в поле "Когда привезти самокат" текущую дату
+     */
+    public void setOrderDateField() {
+        driver.findElement(orderDateField).click();
+        driver.findElement(orderDate).click();
+    }
+
+    /**
+     * Метод выбирает в поле "Срок аренды" "сутки"
+     */
+    public void setRentTimeFieldField() {
+        driver.findElement(rentTimeField).click();
+        driver.findElement(rentTime).click();
+    }
+
+    /**
+     * Метод нажимает кнопку "Заказать" на второй форме заказа
+     */
+    public void clickOrderButton() {
+        driver.findElement(orderButton).click();
+    }
+
+    /**
+     * Метод нажимает кнопку "Да" на всплывающем окне "Хотите оформить заказ?"
+     */
+    public void clickConfirmButton() {
+        driver.findElement(confirmButton).click();
+    }
+
+    /**
+     * Метод возвращает текст аголовока всплывающего окна об успешном формировании заказа
+     */
+    public String getCompleteOrderTitleText() {
+        return driver.findElement(completeOrderFormTitle).getText();
+    }
+
+
+    /**
+     * Метод формирует заказ самоката через кнопку "Заказать" в хедере страницы
+     * @param newFirstName
+     * @param newFamilyName
+     * @param newAddress
+     * @param newTelNumber
+     */
+    public void createOrderViaHeaderOrderButton(
+            String newFirstName, String newFamilyName,
+            String newAddress, String newTelNumber
+            ) {
+        setFirstName(newFirstName);
+        setFamilyName(newFamilyName);
+        setAddress(newAddress);
+        setMetroStation();
+        setTelNumber(newTelNumber);
+        clickNextButton();
+        setOrderDateField();
+        setRentTimeFieldField();
+        clickOrderButton();
+        clickConfirmButton();
+    }
+}
