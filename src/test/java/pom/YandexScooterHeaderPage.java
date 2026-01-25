efa97f3472484a828b0fe04c6f251e379eb7e7e0
Kostin Ivan <mrironheart@gmail.com>
2026-01-25 19:25:29 +0300

    Добавил POM-класс для хедера

---
+++ src/test/java/pom/YandexScooterHeaderPage.java
@@ -0,0 +1,30 @@
+package pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class YandexScooterHeaderPage {
+    private final WebDriver driver;
+    private final By faqSpoilerTitle = By.className("Button_Button__ra12g");
+
+    public YandexScooterHeaderPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    /**
+     * Метод нажимает на нопку "Заказать" в хедере
+     */
+    public void clickHeaderOrderButton() {
+        WebElement element = driver.findElement(faqSpoilerTitle);
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
+        new WebDriverWait(driver, Duration.ofSeconds(2))
+                .until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
+}
