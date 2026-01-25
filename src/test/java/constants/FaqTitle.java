b42a645697c8ff410c8ead6f2fd036c991f84bea
Kostin Ivan <mrironheart@gmail.com>
2026-01-25 18:54:49 +0300

    Добавил константы

---
+++ src/test/java/constants/FaqTitle.java
@@ -0,0 +1,12 @@
+package constants;
+
+public class FaqTitle {
+    public static final String TITLE_PRICE = "Сколько это стоит? И как оплатить?";
+    public static final String TITLE_MULTIPLE_SCOOTERS = "Хочу сразу несколько самокатов! Так можно?";
+    public static final String TITLE_RENT_TIME = "Как рассчитывается время аренды?";
+    public static final String TITLE_ORDER_TODAY = "Можно ли заказать самокат прямо на сегодня?";
+    public static final String TITLE_PROLONGATION = "Можно ли продлить заказ или вернуть самокат раньше?";
+    public static final String TITLE_CHARGER = "Вы привозите зарядку вместе с самокатом?";
+    public static final String TITLE_CANCEL_ORDER = "Можно ли отменить заказ?";
+    public static final String TITLE_SERVICE_AREA = "Я жизу за МКАДом, привезёте?";
+}
