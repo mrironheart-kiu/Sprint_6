d9dc7fcdfbdad0326732d055dcaef5a313338f43
Kostin Ivan <mrironheart@gmail.com>
2026-01-26 01:19:09 +0300

    Очистка неиспользуемого

--- src/test/java/YandexScooterTests.java
+++ src/test/java/YandexScooterTests.java
@@ -1,7 +1,6 @@
 import constants.Browser;
 import factory.WebDriverFactory;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.WebDriver;
@@ -22,10 +21,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class YandexScooterTests {
     private WebDriver driver;
-
-//    @BeforeEach
-//    void setUp() {
-//    }
 
     @ParameterizedTest
     @MethodSource("testdata.ParameterizedTestData#faqTestData")
