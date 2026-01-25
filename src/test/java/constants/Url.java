78523d1f6760028a1c96f2316eaf33d482a58a54
Kostin Ivan <mrironheart@gmail.com>
2026-01-25 19:24:55 +0300

    Добавил URL страницы с формированием заказа

--- src/test/java/constants/Url.java
+++ src/test/java/constants/Url.java
@@ -1,5 +1,6 @@
 package constants;
 
 public class Url {
-    public static final String URL_MAIN_PAGE = "https://qa-scooter.praktikum-services.ru/";
+    public static final String URL_MAIN_PAGE = "https://qa-scooter.praktikum-services.ru";
+    public static final String URL_ORDER_PAGE = "https://qa-scooter.praktikum-services.ru/order";
 }
