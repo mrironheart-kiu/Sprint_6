addcc042974a48d91b8a0d7dfd919fc9eb1fca6a
Kostin Ivan <mrironheart@gmail.com>
2026-01-26 11:17:02 +0300

    Добавил константы текстов ошибок формы заказа

---
+++ src/test/java/constants/ErrorMessage.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class ErrorMessage {
+    public static final String MESSAGE_ERROR_ORDER_FIRST_NAME = "Введите корректное имя";
+    public static final String MESSAGE_ERROR_ORDER_FAMILY_NAME = "Введите корректную фамилию";
+    public static final String MESSAGE_ERROR_ORDER_ADDRESS = "Введите корректный адрес";
+    public static final String MESSAGE_ERROR_ORDER_METRO_STATION = "Выберите станцию";
+    public static final String MESSAGE_ERROR_ORDER_PHONE = "Введите корректный номер";
+}
