230151e6a43d86bafcec5340e95b9fb2cbf30813
Kostin Ivan <mrironheart@gmail.com>
2026-01-26 12:44:03 +0300

    Добавил URL страницы проверки заказа

--- src/test/java/constants/Url.java
+++ src/test/java/constants/Url.java
@@ -3,5 +3,6 @@ package constants;
 public class Url {
     public static final String URL_MAIN_PAGE = "https://qa-scooter.praktikum-services.ru";
     public static final String URL_ORDER_PAGE = "https://qa-scooter.praktikum-services.ru/order";
-    public static final String URL_YANDEX_MAIN_PAGE = "https://dzen.ru/?yredirect=true";
+    public static final String URL_YANDEX_MAIN_PAGE = "https://dzen.ru";
+    public static final String URL_TRACK_ORDER_PAGE = "https://qa-scooter.praktikum-services.ru/track";
 }
