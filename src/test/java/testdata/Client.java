0917a129cfcccecc11cfeca2b76d80d94431c051
Kostin Ivan <mrironheart@gmail.com>
2026-01-29 22:11:30 +0300

    Добавил аннтоции Lombok в класс Client

--- src/test/java/testdata/Client.java
+++ src/test/java/testdata/Client.java
@@ -1,31 +1,15 @@
 package testdata;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+@Getter
+@Builder(toBuilder=true)
+@ToString
 public class Client {
     private final String firstName;
     private final String familyName;
     private final String address;
     private final String phoneNumber;
-
-    public Client(String firstName, String familyName, String address, String phoneNumber) {
-        this.firstName = firstName;
-        this.familyName = familyName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getFamilyName() {
-        return familyName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
 }
